7835b1745b7cb619210013e905631001fa453481
Michael Cole <colemichaelj@yahoo.com>
2020-07-26 18:25:00 -0500

    Loops

---
+++ main.js
@@ -0,0 +1,63 @@
+// do...while from 1-1000
+let n = 0;
+while(n<=1000){
+  n++;
+  console.log(n);
+}
+
+// person object
+let person = {
+  firstName: "Jane",
+  lasyName: "Doe",
+  birthDate: "Jan 5, 1925",
+  gender: "female",
+};
+console.log(person);
+//for..in loop and if statement for odd birthday
+for (let prop in person) {
+  if (prop === "birthDate") {
+  let year = parseInt(person["birthDate"].slice(-4));
+  (year % 2 !== 0)
+      console.log(year, "is odd");
+  }
+}
+
+
+//arrayOfPersons with multiple objects
+
+let arrayOfPersons= [
+{
+    firstName: "Jill",
+    lastName: "Scott",
+    birthDate: "Apr 4, 1972",
+    gender: "female",
+},
+{
+    firstName: "Taye",
+    lastName: "Digs",
+    birthDate: "Jan 2, 1971",
+    gender: "male",
+},
+{
+    firstName: "Remy",
+    lastName: "Martin",
+    birthDate: "Feb 7, 1806",
+    gender: "male",
+}
+];
+console.log(arrayOfPersons);
+
+//.map to map over the arrayOfPersons
+arrayOfPersons.map(item => {
+  return item;
+});
+
+
+//.filter only males in the array
+let men = arrayOfPersons.filter(arrayOfPersons => arrayOfPersons.gender === "male");
+console.log(men);
+
+
+//.filter people born before jan 1, 1990
+let old = arrayOfPersons.filter(birthDate => birthDate["birthDate"].slice(-4) < "1990");
+console.log(old);
